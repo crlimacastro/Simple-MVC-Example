a49542f6da58544199b6bd04f237dfc5d381ecbb
crlimacastro <crlimacastro@gmail.com>
2021-03-24 00:14:48 -0400

    Finished example

---
+++ server/models/Cat.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+let CatModel = {};
+
+const CatSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  bedsOwned: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  createdDate: {
+    type: Date,
+    defaults: Date.now,
+  },
+});
+
+CatSchema.statics.findByName = (name, callback) => {
+  const search = {
+    name,
+  };
+
+  return CatModel.findOne(search, callback);
+};
+
+CatModel = mongoose.model('Cat', CatSchema);
+
+module.exports = {
+  CatModel,
+  CatSchema,
+};
